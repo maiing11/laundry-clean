7a46bd96179e704396785ed01ee160280d7b04f8
maiing11 <maherboleax@gmail.com>
2023-11-12 22:12:10 +0700

    feat: Setup the golang project

---
+++ initializers/loadEnv.go
@@ -0,0 +1,22 @@
+package initializers
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	PostgresURL string `mapstructure:"POSTGRES_URL"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+
+}
